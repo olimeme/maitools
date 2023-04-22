87872885592737d20185ec74555d1afcc95212f7
olimeme <alibek.mombekov@mail.ru>
2023-04-22 17:56:51 +0600

    markdown: fix code bg and fg for different color modes

--- src/pages/MarkdownPage.tsx
+++ src/pages/MarkdownPage.tsx
@@ -1,20 +1,56 @@
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { Components } from "react-markdown";
 import remarkGfm from "remark-gfm";
 import MotionWrapper from "../components/MotionWrapper";
-import { Box, Flex, Heading, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Code,
+  Flex,
+  Heading,
+  Text,
+  Textarea,
+  useColorMode,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import ChakraUIRenderer from "../helpers/chakraUIRenderer";
 import rehypeRaw from "rehype-raw";
 import rehypeHighlight from "rehype-highlight";
+import rehypeSanitize from "rehype-sanitize";
+import { ReactMarkdownProps } from "react-markdown/lib/complex-types";
 
 const MarkdownPage = () => {
   const [markdownValue, setMarkdownValue] = useState("");
+  const { colorMode, toggleColorMode } = useColorMode();
 
   const handleInputChange:
     | React.ChangeEventHandler<HTMLTextAreaElement>
     | undefined = (e) => {
     let inputValue = e.target.value;
     setMarkdownValue(inputValue);
+  };
+
+  const codeChakraUIElementRenderer: Components = {
+    code: ({ inline, children, className }) => {
+      if (inline) {
+        return (
+          <Text
+            as="kbd"
+            children={children}
+            bgColor={colorMode === "light" ? "gray.300" : "whiteAlpha.300"}
+            color={colorMode === "light" ? "gray.900" : "whiteAlpha.900"}
+          />
+        );
+      }
+
+      return (
+        <Code
+          className={className}
+          whiteSpace="break-spaces"
+          display="block"
+          w="full"
+          children={children}
+        />
+      );
+    },
   };
 
   return (
@@ -36,8 +72,8 @@ const MarkdownPage = () => {
         <Box flex={1} px={8}>
           <ReactMarkdown
             remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw, rehypeHighlight]}
-            components={ChakraUIRenderer()}
+            rehypePlugins={[rehypeRaw, rehypeHighlight, rehypeSanitize]}
+            components={ChakraUIRenderer(codeChakraUIElementRenderer)}
             children={markdownValue}
           />
         </Box>
