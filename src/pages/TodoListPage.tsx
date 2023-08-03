a4facf2652e8824be739bee36ef89d86b8721abc
olimeme <alibek.mombekov@mail.ru>
2023-08-03 11:53:45 +0600

    initial commit

---
+++ src/pages/TodoListPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MotionWrapper from "../components/MotionWrapper";
+import { Heading } from "@chakra-ui/react";
+
+const TodoListPage = () => {
+  return (
+    <MotionWrapper>
+      <Heading>Todo list page</Heading>
+    </MotionWrapper>
+  );
+};
+
+export default TodoListPage;
