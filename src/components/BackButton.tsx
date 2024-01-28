05d30a3686e82737a104f38bb83c8c87c1ea5bea
olimeme <olisanmomyn@proton.me>
2024-01-28 14:51:57 +0000

    Update SpacedRep components and add DeckPage

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -2,11 +2,13 @@ import { Button, ButtonProps } from "@chakra-ui/button";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 
-interface BackButtonProps extends ButtonProps {}
+interface BackButtonProps extends ButtonProps {
+  to?: string;
+}
 
-const BackButton = ({ ...props }: BackButtonProps) => {
+const BackButton = ({ to, ...props }: BackButtonProps) => {
   return (
-    <Link to="/">
+    <Link to={to ?? "/"}>
       <Button variant="ghost" mb={4} leftIcon={<ArrowBackIcon />} {...props}>
         Back
       </Button>
