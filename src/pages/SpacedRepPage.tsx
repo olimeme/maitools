b8f6e612e07252903b71cbb102bddc215c00463e
olimeme <olisanmomyn@proton.me>
2024-02-07 11:23:20 +0000

    Add CheckAuth component and implement authentication in SpacedRepPage

--- src/pages/SpacedRepPage.tsx
+++ src/pages/SpacedRepPage.tsx
@@ -21,6 +21,8 @@ import { AddIcon } from "@chakra-ui/icons";
 import { BsListUl } from "react-icons/bs";
 import { RxDashboard } from "react-icons/rx";
 import { getInitialStateFromLocalStorage } from "../helpers/getInitialStateFromLocalStorage";
+import AuthService from "../services/AuthService";
+import CheckAuth from "./CheckAuth";
 
 export type DeckViewTypes = "list" | "gallery";
 
@@ -28,6 +30,7 @@ const SpacedRepPage = () => {
   const [deckList, setDeckList] = useState<Array<any>>(
     () => getInitialStateFromLocalStorage("deckList", []) as Array<any>
   );
+
   const [currentDeckView, setCurrentDeckView] = useState<DeckViewTypes>(
     () =>
       getInitialStateFromLocalStorage(
@@ -66,11 +69,12 @@ const SpacedRepPage = () => {
 
   return (
     <MotionWrapper>
-      <ButtonGroup size={"sm"}>
-        <Button leftIcon={<AddIcon />} size={"sm"} onClick={onOpen}>
-          Add deck
-        </Button>
-        {/* <ButtonGroup isAttached size={"sm"}>
+      <CheckAuth>
+        <ButtonGroup size={"sm"}>
+          <Button leftIcon={<AddIcon />} size={"sm"} onClick={onOpen}>
+            Add deck
+          </Button>
+          {/* <ButtonGroup isAttached size={"sm"}>
           <IconButton
             icon={<BsListUl />}
             aria-label="list view"
@@ -88,53 +92,54 @@ const SpacedRepPage = () => {
             }}
           ></IconButton>
         </ButtonGroup> */}
-        <Button
-          onClick={() => {
-            setDeckList([]);
-            localStorage.removeItem("deckList");
-            localStorage.removeItem("currentDeckView");
-          }}
-        >
-          clear
-        </Button>
-      </ButtonGroup>
-      <DeckDashboard
-        cards={deckList}
-        view={currentDeckView}
-        handleDeleteDeck={handleDeleteDeck}
-      />
+          <Button
+            onClick={() => {
+              setDeckList([]);
+              localStorage.removeItem("deckList");
+              localStorage.removeItem("currentDeckView");
+            }}
+          >
+            clear
+          </Button>
+        </ButtonGroup>
+        <DeckDashboard
+          cards={deckList}
+          view={currentDeckView}
+          handleDeleteDeck={handleDeleteDeck}
+        />
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add deck</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Heading size={"sm"}>Deck name:</Heading>
-            <Heading size={"xs"} color={"grey"} my={2}>
-              This is where your cards are going to be stored.
-            </Heading>
-            <Input
-              type="text"
-              value={inputDeckName}
-              autoFocus
-              onChange={(val) => setInputDeckName(val.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") handleCreateDeck();
-              }}
-            />
-          </ModalBody>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add deck</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Heading size={"sm"}>Deck name:</Heading>
+              <Heading size={"xs"} color={"grey"} my={2}>
+                This is where your cards are going to be stored.
+              </Heading>
+              <Input
+                type="text"
+                value={inputDeckName}
+                autoFocus
+                onChange={(val) => setInputDeckName(val.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") handleCreateDeck();
+                }}
+              />
+            </ModalBody>
 
-          <ModalFooter>
-            <Button mr={3} onClick={handleCreateDeck}>
-              Add
-            </Button>
-            <Button variant="ghost" onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+            <ModalFooter>
+              <Button mr={3} onClick={handleCreateDeck}>
+                Add
+              </Button>
+              <Button variant="ghost" onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </CheckAuth>
     </MotionWrapper>
   );
 };
