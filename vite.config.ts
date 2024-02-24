c6dc5a45e7c36f76da4a056a5eeb7d623a0b3c62
olimeme <olisanmomyn@proton.me>
2024-02-24 01:58:35 +0000

    fix env

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_API_BASE_URL,
   assetsInclude: ["**/*.mp3"],
 });
