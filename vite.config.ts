f224eb69b57df64f03820e3bab5ef0ca8013acda
olimeme <olisanmomyn@proton.me>
2024-02-24 02:07:31 +0000

    Refactor login URL and define app environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_API_BASE_URL,
+  define: {
+    __APP_ENV__: process.env.VITE_API_BASE_URL,
+  },
   assetsInclude: ["**/*.mp3"],
 });
