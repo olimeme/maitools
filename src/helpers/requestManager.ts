b7a5e13f04cc1e9642a6b1e9a6cbe2ad88611153
olimeme <olisanmomyn@proton.me>
2024-02-26 21:00:00 +0000

    Add requestManager helper function and refactor SpacedRepService

---
+++ src/helpers/requestManager.ts
@@ -0,0 +1,23 @@
+import authAxios from "./authAxios";
+
+export const requestManager = async (
+  url: string,
+  data: any,
+  method: "get" | "post" | "update" | "delete" | "put"
+) => {
+  try {
+    const response = await authAxios(url, { data: { ...data }, method });
+    return Promise.resolve(response.data);
+  } catch (error: any) {
+    const message = error.response?.data?.message;
+    if (message) {
+      return Promise.reject(message);
+    }
+    const defaultErr = error.response?.data?.statusText;
+    if (defaultErr) {
+      return Promise.reject(defaultErr);
+    } else {
+      return Promise.reject("Server Error");
+    }
+  }
+};
