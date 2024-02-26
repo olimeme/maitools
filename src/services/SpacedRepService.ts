8d150a64d73ed164d2156156b80585754bdc5990
olimeme <olisanmomyn@proton.me>
2024-02-26 21:00:00 +0000

    Add requestManager helper function and refactor SpacedRepService

--- src/services/SpacedRepService.ts
+++ src/services/SpacedRepService.ts
@@ -1,86 +1,49 @@
-import authAxios from "../helpers/authAxios";
-import { CookieManager } from "../helpers/CookieManager";
-import { ISpacedRepetitionDeck } from "../interfaces/SpacedRepetition/ISpacedRepetitionDeck";
+import { requestManager } from "../helpers/requestManager";
 
 class SpacedRepService {
-
   static async getDeckList() {
-    try {
-      const response = await authAxios.get(`/spaced-rep/get-deck`);
-      return Promise.resolve(response.data);
-    } catch (error: any) {
-      const message = error.response?.data?.message;
-      if (message) {
-        return Promise.reject(message);
-      }
-      const defaultErr = error.response?.data?.statusText;
-      if (defaultErr) {
-        return Promise.reject(defaultErr);
-      } else {
-        return Promise.reject("Server Error");
-      }
-    }
+    return await requestManager(`/spaced-rep/get-deck`, null, "get");
   }
 
   static async createDeck(data: string) {
-    try {
-      const response = await authAxios.post(`/spaced-rep/create-deck`, {
-        deckName: data,
-      });
-      return Promise.resolve(response.data);
-    } catch (error: any) {
-      const message = error.response?.data?.message;
-      if (message) {
-        return Promise.reject(message);
-      }
-      const defaultErr = error.response?.data?.statusText;
-      if (defaultErr) {
-        return Promise.reject(defaultErr);
-      } else {
-        return Promise.reject("Server Error");
-      }
-    }
+    return await requestManager(
+      `/spaced-rep/create-deck`,
+      { deckName: data },
+      "post"
+    );
   }
 
   static async deleteDeck(deckId: string) {
-    try {
-      const response = await authAxios.delete(`/spaced-rep/delete-deck`, {
-        data: { deckId },
-      });
-      return Promise.resolve(response.data);
-    } catch (error: any) {
-      const message = error.response?.data?.message;
-      if (message) {
-        return Promise.reject(message);
-      }
-      const defaultErr = error.response?.data?.statusText;
-      if (defaultErr) {
-        return Promise.reject(defaultErr);
-      } else {
-        return Promise.reject("Server Error");
-      }
-    }
+    return await requestManager(
+      `/spaced-rep/delete-deck`,
+      {
+        deckId: deckId,
+      },
+      "delete"
+    );
   }
 
   static async editDeck(deckId: string, deckName: string) {
-    try {
-      const response = await authAxios.put(`/spaced-rep/update-deck`, {
+    return await requestManager(
+      `/spaced-rep/update-deck`,
+      {
         deckId,
         deckName,
-      });
-      return Promise.resolve(response.data);
-    } catch (error: any) {
-      const message = error.response?.data?.message;
-      if (message) {
-        return Promise.reject(message);
-      }
-      const defaultErr = error.response?.data?.statusText;
-      if (defaultErr) {
-        return Promise.reject(defaultErr);
-      } else {
-        return Promise.reject("Server Error");
-      }
-    }
+      },
+      "put"
+    );
+  }
+
+  static async createCard(deckId: string, front: string, back: string) {
+    return await requestManager(
+      `/spaced-rep/update-deck`,
+      {
+        deckId,
+        front,
+        back,
+      },
+      "post"
+    );
   }
 }
 
