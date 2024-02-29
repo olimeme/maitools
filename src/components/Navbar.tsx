3c40a5d61eeed013d7f01eaa6bdd565e5dd27cb5
olimeme <olisanmomyn@proton.me>
2024-02-29 21:24:18 +0000

    Remove tools drawer for drapdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,74 @@ import {
   Box,
   Button,
   Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Spacer,
   Text,
   Tooltip,
   useDisclosure,
 } from "@chakra-ui/react";
+import {
+  MdDashboard,
+  MdOutlineViewKanban,
+  MdTimer,
+  MdList,
+  MdRepeat,
+} from "react-icons/md";
 import { DarkModeSwitch } from "./DarkMode";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { PomodoroTimerContext } from "../contexts/PomodoroTimerContext";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
 import NavigationDrawer from "./Drawer/NavigationDrawer";
 import useNavbarLoginStatus from "../hooks/useNavbarLoginStatus";
+import { TDrawerItemType } from "../data/drawerItems";
+import { FaMarkdown } from "react-icons/fa";
 const Navbar = () => {
   const {
     data: { displayTimeRemaining, isPomodoroPageOpen, isActive },
   } = useContext(PomodoroTimerContext);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { isLoggedIn, logout } = useNavbarLoginStatus();
+
+  const drawerItems: TDrawerItemType[] = [
+    {
+      id: 1,
+      icon: <FaMarkdown />,
+      link: "/markdown",
+      name: "Markdown",
+    },
+    {
+      id: 2,
+      icon: <MdTimer />,
+      link: "/pomodoro-timer",
+      name: "Pomodoro timer",
+    },
+    // {
+    //   id: 3,
+    //   link: "/whiteboard",
+    //   name: "Whiteboard",
+    // },
+    {
+      id: 4,
+      icon: <MdList />,
+      link: "/todo-list",
+      name: "To do list",
+    },
+    {
+      id: 5,
+      icon: <MdOutlineViewKanban />,
+      link: "/kanban",
+      name: "Kanban board",
+    },
+    {
+      id: 6,
+      icon: <MdRepeat />,
+      link: "/spaced-repetition",
+      name: "Spaced repetition",
+    },
+  ];
 
   return (
     <Flex pos={"absolute"} zIndex={1} w={"100%"} p={4} gap={4}>
@@ -41,13 +92,32 @@ const Navbar = () => {
         </Tooltip>
       )}
       <Box>
-        <Button
+        {/* <Button
           variant={"ghost"}
           onClick={onOpen}
           rightIcon={<HamburgerIcon />}
         >
           tools
-        </Button>
+        </Button> */}
+        <Menu>
+          <MenuButton
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+            variant={"ghost"}
+          >
+            Tools
+          </MenuButton>
+          <MenuList>
+            {drawerItems.map((item) => (
+              <Link to={item.link} key={item.id}>
+                <MenuItem>
+                  {item.icon}
+                  <Text ml={2}>{item.name}</Text>
+                </MenuItem>
+              </Link>
+            ))}
+          </MenuList>
+        </Menu>
         <NavigationDrawer onClose={onClose} isOpen={isOpen} />
       </Box>
       <DarkModeSwitch />
--- src/helpers/CookieManager.ts
+++ src/helpers/CookieManager.ts
@@ -2,7 +2,7 @@ export class CookieManager {
   public static setCookie(name: string, value: string) {
     let expires = "";
     const date = new Date();
-    date.setTime(date.getTime() + 3 * 60 * 60 * 1000);
+    date.setTime(date.getTime() + 60 * 60 * 1000);
     expires = `; expires=${date.toUTCString()}`;
     document.cookie = `${name}=${value || ""}${expires}; path=/`;
   }
