de77533c649671cd3b11c04e3560d210aeb2b96e
olimeme <olisanmomyn@proton.me>
2024-03-07 11:18:30 +0000

    add export options to markdown page

---
+++ src/data/exportOptions.ts
@@ -0,0 +1,14 @@
+export const exportOptions = [
+  {
+    id: 1,
+    name: "PDF",
+    icon: "📄",
+    format: "pdf",
+  },
+  {
+    id: 2,
+    name: "TXT",
+    icon: "📝",
+    format: "txt",
+  },
+];
